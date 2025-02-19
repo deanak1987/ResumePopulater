fe09a4d04c49edeaef15fb1c9ce2f5c048ef2152
Dean <dean.a.kelley@gmail.com>
2025-02-19 14:30:54 -0800

    Making generic info for public facing files

---
+++ db_loader_generic.py
@@ -0,0 +1,24 @@
+from db_manager import add_education, add_coursework, add_certification, add_publication, add_personal_info, get_certifications, get_education, get_publications, get_personal_info, get_education_with_coursework
+if __name__ == "__main__":
+    add_personal_info("John Smith", "john.smith@email.com", "555-555-5555",
+                      "linkedin.com/in/john-smith", "github.com/john-smith", None)
+
+
+    get_personal_info()
+
+    add_education(1, "Associate's of Art", "College", "Quarter", 2015, 3.75)
+
+    # OC Courses
+    add_coursework(1, "General Chemistry Prep", "CHEM 139", "Autumn", 2012,3.5, 5)
+    add_coursework(1, "Precalculus I: Algebra", "MATH 141", "Autumn", 2012,3.1, 5)
+    add_coursework(1, "Spanish I ", "SPAN 121", "Autumn", 2012,4, 5)
+    add_coursework(1, "General Chemistry I", "CHEM 141", "Winter", 2013,3.8, 5)
+
+    add_publication(1, "Creating Cool Stuff.", "smith, J", 2024, "Cool Stuff", "2024(3)", "126–221")
+
+    add_certification(1, "Cool Dude", "Studs", 2017, None, "Cool Guys")
+    # Fetch education with coursework for Person ID = 1
+    get_education(1)
+    get_publications(1)
+    get_certifications(1)
+
--- job_posting_scraper.py
+++ job_posting_scraper.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
